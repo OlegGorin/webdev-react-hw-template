cf00ed06e1fd8aad2af2ef4b9a862223a050ce22
OlegGorin <olgorin@rambler.ru>
2024-07-24 10:41:18 +0200

    Commit changes in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 import classNames from "classnames";
 import Navigation from "@/components/navigation/Navigation";
@@ -6,7 +5,7 @@ import Sidebar from "@/components/sidebar/Sidebar";
 import Playerbar from "@/components/playerbar/Playerbar";
 import { Search } from "@/components/centerblock/search/Search";
 import { Playlist } from "@/components/centerblock/playlist/Playlist";
-import { getTracksAll } from "./api/tracks";
+import { getTracksAll } from "@/app/api/tracks";
 import { TrackType } from "@/Types/track";
 import { Filter } from "@/components/filter/Filter";
 
